bfe64cd409e88663c64afd3d534e95e3f8010030
starbeast <bcdkey@gmail.com>
2014-02-04 11:46:01 +0300

    cosmetic changes and sample structuring

--- SimpleFSProject/Program.fs
+++ SimpleFSProject/Program.fs
@@ -1,28 +1,46 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-open System
+﻿open System
 open System.IO
+
+//discriminated unions
+
+type Suit =
+    |Spade
+    |Club
+    |Diamond
+    |Heart    
+
+type Card =
+    |Ace of Suit
+    |Queen of Suit
+    |King of Suit
+    |Jack of Suit
+    |ValueCard of int * Suit
+
+type BinaryTree =
+    | Node of int * BinaryTree * BinaryTree
+    | Empty
 
 type Statement =
         | Print of string
         | Sequence of Statement * Statement
         | IfStmt of Expression * Statement * Statement
-    // Program expressions
+    // Mutual recursion needs and operator
     and Expression =
         | Integer of int
         | LessThan of Expression * Expression
         | GreaterThan of Expression * Expression
 
+//measures metadata
+[<Measure>] type Meters
+[<Measure>] type Seconds
+
 [<EntryPoint>]
 let main (args : string[]) = 
-    if args.Length <> 2 then
-        failwith "AAAA, error!"
-    let greeting, thing = args.[0], args.[1]
-    let timeOfDay = DateTime.Now.ToString("hh:mm tt")
-    printfn "%s, %s at %s" greeting thing timeOfDay
+    //partition and filter functions of the List class emulation
     let partition (func: 'a -> bool) (lst: 'a list) =        
         (List.filter func lst, List.filter (fun vl -> not (func vl)) lst)
     let x, y = partition (fun vl -> vl % 3 = 0) [1..2..20]
+    //using |> pipeline
     let filter func lst =
         let rec inner innerF innerLst innerRes =
             if List.length innerLst = 0 then
@@ -39,6 +57,7 @@ let main (args : string[]) =
                         (List.tail innerLst)
                         innerRes
         List.rev (inner func lst [])
+    //folder size imperative way
     let sizeOfFolder folder =
         let filesInFolder : string [] =
             Directory.GetFiles(
@@ -58,21 +77,29 @@ let main (args : string[]) =
         printf "total size %d" totalSize
         totalSize
     ignore (sizeOfFolder @"D://Projects")
-//    let sizeOfFolderComposed =
-//        let getFiles folder =
-//            Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories)
-//        getFiles
-//        >> Array.map (fun file -> new FileInfo(file))
-//        >> Array.map (fun info -> info.Length)
-//        >> Array.sum
+
+    //using >> composition operator
+    let sizeOfFolderComposed =
+        let getFiles folder =
+            Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories)
+        getFiles
+        >> Array.map (fun file -> new FileInfo(file))
+        >> Array.map (fun info -> info.Length)
+        >> Array.sum
+    ignore (sizeOfFolderComposed @"D://Projects")
+
     fun str -> str + " is your string"
     <| sprintf "(%d, %d)" 1 2
     |> printf "format: %s "
 
-    [ [1]; []; [4;5;6]; [3;4]; []; []; []; [9] ]
-    |> List.filter (not << List.isEmpty)
+    ignore (
+        [ [1]; []; [4;5;6]; [3;4]; []; []; []; [9] ]
+        |> List.filter (not << List.isEmpty)
+        )
+
     let xx = (<<) not List.isEmpty
 
+    //high-low game using pattern matching
     let highLowGame () =
         let rng = new Random()
         let secretNumber = rng.Next() % 100
@@ -92,6 +119,8 @@ let main (args : string[]) =
                    highLowGameStep()
         // Begin the game
         highLowGameStep()
+
+    //DSL program using discriminated unions
     let program =
         IfStmt(
             GreaterThan(
@@ -103,10 +132,33 @@ let main (args : string[]) =
                 Print("greater than 1")
             )
         )
+
+    //pattern matching on lists
     let rec testing l =
         match l with
         | [] -> false        
         | _ :: tail -> 
                 printf "still having tail"
                 testing tail
+
+    //list yield filling
+    let deckOfCards =
+        [
+            for suit in [ Spade; Club; Heart; Diamond ] do
+                yield Ace(suit)
+                yield King(suit)
+                yield Queen(suit)
+                yield Jack(suit)
+                for value in 2 .. 10 do
+                    yield ValueCard(value, suit)
+        ]
+    
+    //Tree structure printing using pattern matching and discriminated unions
+    let rec printInOrder tree =
+        match tree with
+        | Node (data, left, right) -> 
+            printInOrder left
+            printfn "Node %d" data
+            printInOrder right
+        | Empty -> ()
     0
